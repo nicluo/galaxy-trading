1d75782706f243819fbbb4321ea10d6253fccbaa
Nicholas Luo <nicholasluo@gmail.com>
2017-10-28 13:17:08 +0800

    Add Redux Boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import reducer from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/reducer.js
@@ -0,0 +1,5 @@
+const reducer = (state = undefined, action) => {
+  return state;
+}
+
+export default reducer;
