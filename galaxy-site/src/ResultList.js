c2f969a797c29f17a899884d49f3d7ce51adb1bb
Nicholas Luo <nicholasluo@gmail.com>
2017-11-03 17:02:32 +0800

    Update ResultList to show statements dynamically

--- galaxy-site/src/ResultList.js
+++ galaxy-site/src/ResultList.js
@@ -1,32 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const ResultList = () => (
-  <div className="container my-4">
-    <div className="card my-2">
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">
-          <strong className="h4">
-            how much wood could a woodchuck chuck if a woodchuck could chuck wood ?
-          </strong>
-        </li>
-        <li className="list-group-item list-group-item-danger">
-          I have no idea what you are talking about
-        </li>
-      </ul>
+const ResultList = ({statements}) => {
+  const cards = statements.map((s) => {
+    const classes = classNames('list-group-item', {
+      'list-group-item-danger': s.type === 'error',
+      'list-group-item-success': s.type === 'success'
+    });
+    return (
+      <div className="card my-2" key={s.id}>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <strong className="h4">
+              {s.query}
+            </strong>
+          </li>
+          <li className={classes}>
+            {s.message}
+          </li>
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div className="container my-4">
+      { cards.length > 0 ? cards : DefaultMessage }
     </div>
-    <div className="card my-2">
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">
-          <strong className="h4">
-            how much is pish tegj glob glob ?
-          </strong>
-        </li>
-        <li className="list-group-item list-group-item-success">
-          pish tegj glob glob is 42
-        </li>
-      </ul>
-    </div>
+  );
+};
+
+const DefaultMessage = (
+  <div className="card my-2">
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">
+        <strong className="h4">
+          Welcome!
+        </strong>
+      </li>
+      <li className="list-group-item list-group-item-info">
+        Submit a query to get started!
+      </li>
+    </ul>
   </div>
-);
+)
+
+ResultList.propTypes = {
+  statements: PropTypes.array.isRequired
+};
 
 export default ResultList;
+
