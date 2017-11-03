ce73b35e20de96ad753d6ee1f06afa3356f5ffd4
Nicholas Luo <nicholasluo@gmail.com>
2017-11-03 17:02:47 +0800

    Update Form component to keep track of input and call onSubmit handler

--- galaxy-site/src/Form.js
+++ galaxy-site/src/Form.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export const Form = () => (
-  <div className="container my-4">
-    <div className="form-group">
-      <input type="text" className="form-control form-control-lg" id="query" aria-describedby="query" placeholder="how much is pish pish Iron ?" autoFocus="" />
-    </div>
-  </div>
-);
+class Form extends Component {
+  state = {
+    input: ''
+  };
+
+  handleChange = (e) =>  {
+    this.setState({ input: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.input);
+  };
+
+  render() {
+    const { input } = this.state;
+    return (
+      <div className="container my-4">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input type="text" className="form-control form-control-lg" id="query" aria-describedby="query" placeholder="how much is pish pish Iron ?" autoFocus="" value={input} onChange={this.handleChange} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+Form.PropTypes = {
+  onSubmit: PropTypes.func.isRequired
+};
 
 export default Form;
