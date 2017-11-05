4c5dc5c359e9a10e370724daf43d2e5f3982a686
Nicholas Luo <nicholasluo@gmail.com>
2017-11-05 12:17:20 +0800

    Add unit testing for resources, including tests for transitive closure

---
+++ galaxy-api-parser/api/resources.test.js
@@ -0,0 +1,112 @@
+import Resources from './resources';
+
+describe('Resources', () => {
+  test('getResourcesList should return empty array', () => {
+    const r = new Resources();
+    expect(r.getResourcesList()).toEqual([]);
+  });
+
+  test('addResource should add new item into resourceList', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    expect(r.getResourcesList()).toEqual(['iron']);
+  });
+
+  test('addResource should not add duplicate item into resourceList', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    r.addResource('Iron');
+    expect(r.getResourcesList()).toEqual(['iron']);
+  });
+
+  test('addResource should append new item into resourceList', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    r.addResource('Credits');
+    expect(r.getResourcesList()).toEqual(['iron', 'credits']);
+  });
+
+  test('addResource should append new item into resourceList', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    r.addResource('Credits');
+    expect(r.getResourcesList()).toEqual(['iron', 'credits']);
+  });
+
+  test('queryRelation should return null in case of unrelated query', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    r.addResource('Credits');
+    expect(r.queryRelation(10, 'Iron', 'Credits')).toBeNull();
+  });
+
+  test('queryRelation should return results in case of identity query', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    expect(r.queryRelation(10, 'Iron', 'Iron')).toEqual([
+      {resource: 'Iron', amount: 10},
+      {resource: 'Iron', amount: 10},
+      ]);
+  });
+
+  test('queryRelation should return results using simple relations', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    r.addResource('Credits');
+    r.addResourceRelation(10, 'Iron', 50, 'Credits');
+    expect(r.queryRelation(20, 'Iron', 'Credits')).toEqual([
+      {resource: 'Iron', amount: 20},
+      {resource: 'Credits', amount: 100},
+    ]);
+  });
+
+  test('queryRelation should return results using simple inverse relations', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    r.addResource('Credits');
+    r.addResourceRelation(10, 'Iron', 50, 'Credits');
+    expect(r.queryRelation(100, 'Credits', 'Iron')).toEqual([
+      {resource: 'Credits', amount: 100},
+      {resource: 'Iron', amount: 20},
+    ]);
+  });
+
+  test('queryRelation should return results using simple transitive relations', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    r.addResource('Credits');
+    r.addResource('Dirt');
+    r.addResourceRelation(10, 'Iron', 50, 'Credits');
+    r.addResourceRelation(25, 'Credits', 50, 'Dirt');
+    expect(r.queryRelation(20, 'Iron', 'Dirt')).toEqual([
+      {resource: 'Iron', amount: 20},
+      {resource: 'Credits', amount: 100},
+      {resource: 'Dirt', amount: 200},
+    ]);
+  });
+
+  test('queryRelation should return results using simple inverse relations', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    r.addResource('Credits');
+    r.addResource('Dirt');
+    r.addResourceRelation(10, 'Iron', 50, 'Credits');
+    r.addResourceRelation(25, 'Credits', 50, 'Dirt');
+    expect(r.queryRelation(200, 'Dirt', 'Iron')).toEqual([
+      {resource: 'Dirt', amount: 200},
+      {resource: 'Credits', amount: 100},
+      {resource: 'Iron', amount: 20},
+    ]);
+  });
+
+  test('queryRelation should return null after exhausting transitive relations', () => {
+    const r = new Resources();
+    r.addResource('Iron');
+    r.addResource('Credits');
+    r.addResource('Dirt');
+    r.addResource('Copper');
+    r.addResourceRelation(10, 'Iron', 50, 'Credits');
+    r.addResourceRelation(25, 'Dirt', 50, 'Copper');
+    expect(r.queryRelation(200, 'Dirt', 'Iron')).toBeNull();
+  });
+});
