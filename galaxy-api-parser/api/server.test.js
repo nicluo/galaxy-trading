21939959fb18a389695295d2ef8d6ab51ca5255d
Nicholas Luo <nicholasluo@gmail.com>
2017-11-07 23:48:52 +0800

    Change queries tests to parsers

--- galaxy-api-parser/api/server.test.js
+++ galaxy-api-parser/api/server.test.js
@@ -1,6 +1,7 @@
 import server from './server';
 import Session from './sessions';
 import request from 'supertest';
+import Parser from './parsers';
 
 describe('routes: api', () => {
   test('Should respond with Hello API', async () => {
@@ -11,22 +12,61 @@ describe('routes: api', () => {
   });
 });
 
-describe('routes: queries', () => {
-  test('Should respond to new Query', async () => {
-    const response = await request(server.listen()).post('/queries');
+describe('routes: parsers', () => {
+  test('Should be able to fetch parser', async () => {
+    const p = Parser.createParser();
+    const response = await request(server.listen()).get(`/parsers/${p.id}`);
     expect(response.status).toEqual(200);
     expect(response.type).toEqual('application/json');
-    expect(response.body.data).toEqual('Query');
+    expect(response.body.id).toEqual(p.id);
+    expect(response.body.statements).toEqual([]);
   });
 });
 
-describe('routes: queries/:queryId', () => {
-  test('Should respond to delete Query', async () => {
-    const response = await request(server.listen()).del('/queries/1');
-    expect(response.status).toEqual(200);
-    expect(response.type).toEqual('application/json');
-    expect(response.body.data).toEqual('Query Delete');
+describe('routes: parsers', () => {
+    test('Should respond 404 to invalid fetch parser', async () => {
+        const response = await request(server.listen()).get('/parsers/1');
+        expect(response.status).toEqual(404);
+    });
+});
+
+describe('routes: parsers', () => {
+    test('Should be able to create statement', async () => {
+        const p = Parser.createParser();
+        const response = await request(server.listen()).post(`/parsers/${p.id}/statements`).send({
+          query: 'tegj is L',
+        });
+        expect(response.status).toEqual(200);
+        expect(response.type).toEqual('application/json');
+        expect(response.body.id).toBeTruthy();
+        expect(response.body.query).toEqual('tegj is L');
+        expect(response.body.type).toEqual('success');
+        expect(response.body.message).toBeTruthy();
+    });
+});
+
+describe('routes: parsers', () => {
+    test('Should respond 404 to invalid create statement', async () => {
+        const response = await request(server.listen()).post('/parsers/1/statements');
+        expect(response.status).toEqual(404);
+    });
+});
+
+describe('routes: parsers', () => {
+  test('Should respond to delete request', async () => {
+    const p = Parser.createParser();
+    const s = p.query('tegj is L');
+    const response = await request(server.listen()).del(`/parsers/${p.id}/statements/${s.id}`);
+    expect(response.status).toEqual(202);
+    expect(p.statements.length).toEqual(0);
   });
+});
+
+describe('routes: parsers', () => {
+    test('Should respond 404 to invalid delete request', async () => {
+        const response = await request(server.listen()).del('/parsers/1/statements/1');
+        expect(response.status).toEqual(404);
+    });
 });
 
 describe('routes: session', () => {
