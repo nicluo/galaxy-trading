b7b0c587a64d2fce1fb7cd3a2666a0586e703235
Nicholas Luo <nicholasluo@gmail.com>
2017-11-09 10:34:17 +0800

    Remove redux

--- galaxy-site/src/index.js
+++ galaxy-site/src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './reducer';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-const store = createStore(reducer);
-
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
 
--- galaxy-site/src/reducer.js
+++
@@ -1,5 +0,0 @@
-const reducer = (state = undefined, action) => {
-  return state;
-}
-
-export default reducer;
