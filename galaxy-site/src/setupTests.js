8ea67a384ca2ecaf63558d545236854c3379c1f6
Nicholas Luo <nicholasluo@gmail.com>
2017-11-09 10:05:58 +0800

    Add Sinon and Enzyme for React testing

--- galaxy-site/src/setupTests.js
+++ galaxy-site/src/setupTests.js
@@ -1,4 +1,18 @@
+/**
+ * Configure localStorage mock
+ */
 import 'jest-localstorage-mock';
 
+/**
+ * Configure fetch mock
+ */
 import fetch from 'jest-fetch-mock';
 global.fetch = fetch;
+
+/**
+ * Configure enzyme adapter
+ */
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
